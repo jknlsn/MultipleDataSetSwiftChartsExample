b397f41aff7bad0fbd48e5245a9c6d925d56d839
Jake Nelson <j.n@live.com.au>
2022-07-26 19:25:46 +0100

    Initial Commit

---
+++ TestSwiftCharts/TestSwiftChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftChartsApp.swift
+//  TestSwiftCharts
+//
+//  Created by Jake Nelson on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
